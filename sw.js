0beda48836b9631e5748020477b110bc65b47d04
spidychoipro <spidychoipro@gmail.com>
2026-06-29 16:02:34 +0900

    Remove ALL restore/resume features, self-destruct SW to clear caches, auto-reload on SW update

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-// Service worker disabled - no caching
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) { return Promise.all(keys.map(function(k) { return caches.delete(k); })); })
+    .then(function() { return self.clients.claim(); })
+  );
+});
